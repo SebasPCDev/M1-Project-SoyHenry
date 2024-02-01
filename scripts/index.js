f9d4eb313951ff6a522e63ea8813f0bebc282b37
SebasPCDev <sebpa.16@gmail.com>
2024-02-01 01:17:21 -0500

    Publicación final del repo para el proyecto del M1

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 
-
+//Creación de las clases
 class Activity {
     constructor({ id, title, description, imgUrl }) {
         this.id = id,
@@ -17,7 +17,6 @@ class Repository {
     }
     createActivity(activity) {
         const newActivity = new Activity(activity)
-        console.log(this.activities)
         this.activities.push(newActivity)
 
     }
@@ -25,18 +24,22 @@ class Repository {
         this.activities = this.activities.filter(activity => activity.id !== id)
     }
 }
+
 //Inicializo una nueva instancia de la clase Repository
 const NewRepo = new Repository();
 
-
+//Declaración de la función que agrega una tarjeta
 function addCard(obj) {
 
-    const { title, description, imgUrl } = obj
-
+    const { title, description, imgUrl } = obj // Declaración de variables por destructuting
     const h3 = document.createElement('h3')
     const p = document.createElement('p')
     const img = document.createElement('img')
+    const deleteImg = document.createElement('img')
     img.src = imgUrl;
+    deleteImg.src = "assets/bin.png"
+    deleteImg.className = "delete-img"
+
 
     h3.innerHTML = title;
     p.innerHTML = description;
@@ -49,11 +52,13 @@ function addCard(obj) {
     newCard.appendChild(p);
     newCard.appendChild(divImgContainer);
     divImgContainer.appendChild(img)
+    divImgContainer.appendChild(deleteImg)
     newCard.className = "tarjeta"
+
 
     return newCard;
 }
-
+//Declaración de la función que convierte la tarjeta en HTML
 function convert() {
 
     let contenedor = document.getElementById('tarjetas')
@@ -61,20 +66,34 @@ function convert() {
 
 
     let activities = NewRepo.getAllActivities();
-    let mapping = activities.map(addCard)
+    let mapping = activities.map(addCard)  //La variable mapping guarda un arreglo con los elementos que retorna AddCard
 
-
+    //el bucle foreach recorre cada elemento del mapping y da un appendchild al contendor
     mapping.forEach(element => {
         contenedor.appendChild(element);
     })
 
 }
 
+function deleteImg(id) {
+
+
+    let activityToDelete = NewRepo.getAllActivities();
+    console.log("estoy en delete")
+}
+
+//Declaración de la función manejadora al evento del botón.
 function handler() {
 
+    listaActividades = NewRepo.getAllActivities();
+
+    //extraer lo valores de los inputs
     let nombre = document.getElementById('nombre').value;
     let descripcion = document.getElementById('descripcion').value;
     let imagen = document.getElementById('imagen').value;
+
+    //Esto me valida si algún input está vacío, generando un alert en caso de que informe. 
+    //Si hay algo vacío, detiene el proceso.
     let mensaje = "";
     if (!nombre.trim()) {
         mensaje = mensaje + " nombre,"
@@ -92,10 +111,14 @@ function handler() {
         document.getElementById('formulario').reset();
         return false;
     }
-
     NewRepo.createActivity({ id: Date.now(), title: nombre, description: descripcion, imgUrl: imagen });
     convert();
+
+
     document.getElementById('formulario').reset();
+    //reinicio del formulario
 }
+
+
 
 
